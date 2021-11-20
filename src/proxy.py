ab85d38cb34c95bb6baef68f66aac5de27cfab68
Terraminator <erik1wilhelm@gmail.com>
2021-11-20 19:57:30 +0100

    update

--- src/proxy.py
+++ src/proxy.py
@@ -30,23 +30,23 @@ class Proxy:
 		rc = self.connect(packet)
 		client_socket.send(b"HTTP/1.1 200 Established\r\n\r\n")
 		while True:
-			#try:
-			packet = ""
-			packet = client_socket.recv(4096)
-			if packet != b'':
-				packet = self.filter_packet(packet)
-				rc.send(packet)
-				#print("request:", packet)
-				rsp = rc.recv(8096)
-				client_socket.send(rsp)
-				if rsp != '':
-					#print("response:", rsp)
+			try:
+				packet = b''
+				packet = client_socket.recv(4096)
+				if packet != b'':
+					packet = self.filter_packet(packet)
+					rc.send(packet)
+					#print("request:", packet.decode("utf8"))
+					rsp = rc.recv(8096)
 					client_socket.send(rsp)
-			else:
-				return(0)
+					if rsp != '':
+						print("response:", rsp.decode("utf8"))
+						client_socket.send(rsp)
+				else:
+					return(0)
 				
-			#except Exception as e:
-			#	print(e)
+			except Exception as e:
+				print(e)
 		
 		
 	def get_host(self, packet):
@@ -72,18 +72,14 @@ class Proxy:
 	def connect(self, packet):
 		host, port = self.get_host(packet)
 		rc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		#rc.connect((socket.gethostbyname(host), int(port)))
-		ctx = ssl.create_default_context()
-		ctx = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
-		ctx.verify_mode=ssl.CERT_REQUIRED
-		ctx.check_hostname=True
-		ctx.load_verify_locations("../certs/ca-bundle.pem") #https://letsencrypt.org/certificates/
-		rc = ctx.wrap_socket(rc, server_hostname=str(host))
-		rc.connect((str(host), 443))
-		cert = rc.getpeercert()
-		print(cert)
+		rc.settimeout(10)
+		context = ssl.create_default_context()
+
+		sock = socket.create_connection((str(socket.gethostbyname(host)), int(port)))
+		ssock = context.wrap_socket(sock, server_hostname=host)
+		print(ssock.version())
 			
-		return(rc)
+		return(ssock)
 		
 		
 	def filter_packet(self, packet):
